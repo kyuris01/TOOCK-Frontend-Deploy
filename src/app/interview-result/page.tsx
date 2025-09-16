05c0adae159de128a72ad8afb5a376ccde624dcd
kyuris01 <chrisahn1007@gmail.com>
2025-09-16 07:18:14 +0000

    fix : useSearchParams의 비동기성을 해결하기위해 부모에 Suspense 컴포넌트 추가

--- src/app/interview-result/page.tsx
+++ src/app/interview-result/page.tsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { Suspense } from "react";
 import TopNav from "./components/TopNav";
 import InterviewResultContainer from "./components/InterviewResultContainer";
+import { MoonLoader } from "react-spinners";
 
 const Page = () => {
   return (
     <div className="flex flex-col items-center justify-start w-full h-full bg-white">
       <TopNav />
-      <InterviewResultContainer />
+      <Suspense fallback={<MoonLoader color={"var(--color-blue-950)"} />}>
+        <InterviewResultContainer />
+      </Suspense>
     </div>
   );
 };
