b75015272a2ca67ba587d2645cf262bbc6f274c2
kyuris01 <chrisahn1007@gmail.com>
2025-09-16 07:38:19 +0000

    fix : ssr의 프리렌더 과정에서 browser의 전역 객체를 참조해서 발생하는 오류 수정

--- src/app/interview-setup/page.tsx
+++ src/app/interview-setup/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import InterviewSettingBox from "./components/InterviewSettingBox";
 import TopNav from "./components/TopNav";
 import ReactDOM from "react-dom";
@@ -9,40 +9,47 @@ import InterviewSettingModal from "./components/InterviewSettingModal";
 
 const Page = () => {
   const [isModal, setIsModal] = useState<boolean>(false);
+  const [mounted, setMounted] = useState(false);
 
-  const portal = ReactDOM.createPortal(
-    <AnimatePresence>
-      {isModal && (
-        <>
-          <motion.div
-            key="backdrop"
-            className="fixed inset-0 bg-black z-[999]"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 0.5 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 0.2 }}
-            onClick={() => setIsModal(false)}
-          />
-          <motion.div
-            key="panel"
-            className="fixed top-1/2 left-1/2 
+  useEffect(() => {
+    setMounted(true); // ✅ 브라우저에서만 true
+  }, []);
+
+  const portal =
+    mounted &&
+    ReactDOM.createPortal(
+      <AnimatePresence>
+        {isModal && (
+          <>
+            <motion.div
+              key="backdrop"
+              className="fixed inset-0 bg-black z-[999]"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 0.5 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.2 }}
+              onClick={() => setIsModal(false)}
+            />
+            <motion.div
+              key="panel"
+              className="fixed top-1/2 left-1/2 
     -translate-x-1/2 -translate-y-1/2
     bg-white rounded-2xl shadow-xl
     w-[80%] sm:w-full h-[17rem] max-w-md p-4
     z-[1000]"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ type: "spring", stiffness: 300, damping: 30 }}
-            onClick={(e) => e.stopPropagation()}
-          >
-            <InterviewSettingModal setIsModal={setIsModal} />
-          </motion.div>
-        </>
-      )}
-    </AnimatePresence>,
-    document.body
-  );
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ type: "spring", stiffness: 300, damping: 30 }}
+              onClick={(e) => e.stopPropagation()}
+            >
+              <InterviewSettingModal setIsModal={setIsModal} />
+            </motion.div>
+          </>
+        )}
+      </AnimatePresence>,
+      document.body
+    );
   return (
     <div className="flex flex-col items-center justify-start gap-5 min-h-screen w-full">
       <TopNav />
